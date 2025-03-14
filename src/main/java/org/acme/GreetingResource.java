1f98b5022d022840f69e1a16d81e5718e08f56f8
Jules <jules.delaire.76@gmail.com>
2025-03-14 10:13:11 +0100

    feat: return json in response

---
+++ src/main/java/org/acme/GreetingResource.java
@@ -0,0 +1,16 @@
+package org.acme;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class GreetingResource {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String hello() {
+        return "{\"key\": \"value\"}";
+    }
+}
