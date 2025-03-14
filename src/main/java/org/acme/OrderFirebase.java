bd07da6ab53b3d20c7ddad190dd13d3fc1ec353f
Maxence <maxencewalter1@gmail.com>
2025-03-14 14:59:22 +0100

    fix: get/setter for OrderFirebase

--- src/main/java/org/acme/OrderFirebase.java
+++ src/main/java/org/acme/OrderFirebase.java
@@ -8,4 +8,44 @@ public class OrderFirebase {
     private String product;
     public Date orderDate;
     public Date deliveryDate;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getProduct() {
+        return product;
+    }
+
+    public void setProduct(String product) {
+        this.product = product;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public Date getDeliveryDate() {
+        return deliveryDate;
+    }
+
+    public void setDeliveryDate(Date deliveryDate) {
+        this.deliveryDate = deliveryDate;
+    }
 }
